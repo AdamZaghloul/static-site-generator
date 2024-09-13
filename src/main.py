e3dcefba70113c954ed1b461e52743bbd18b2ea1
AdamZaghloul <adam.zaghloul95@gmail.com>
2024-09-13 07:13:31 -0400

    added generate public function

--- src/main.py
+++ src/main.py
@@ -1,9 +1,33 @@
 from textnode import *
 from conversions import *
+import shutil
+import os.path
 
 def main():
 
-    textnode = TextNode("this is some text", "text type", "www.swag.com")
-    print(textnode.__repr__())
+    #textnode = TextNode("this is some text", "text type", "www.swag.com")
+    #print(textnode.__repr__())
+    generate_public()
+
+def generate_public():
+    if os.path.exists("public"):
+        shutil.rmtree("public")
+    
+    os.mkdir("public")
+    
+    copy_static("static", "public")
+
+def copy_static(source, destination):
+    paths = os.listdir(source)
+
+    for path in paths:
+        if os.path.exists(f"{destination}/{path}"):
+            pass
+        else:
+            if os.path.isfile(f"{source}/{path}"):
+                shutil.copy(f"{source}/{path}", f"{destination}/")
+            else:
+                os.mkdir(f"{destination}/{path}")
+                copy_static(f"{source}/{path}", f"{destination}/{path}")
 
 main()
